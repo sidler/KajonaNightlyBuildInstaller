54899ff2c91c364932ab8e4a00282bd1573797eb
sidler <sidler@mulchprod.de>
2014-11-04 09:36:22 +0100

    the old installation is saved before unpacking the latest build

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ KajonaNightlyBuildInstaller
 ===========================
 
 Downloads the latest Kajona build to the current directory and installs the system using sqlite directly.
+
+Call it either by commandline
+
+```
+php -f install.php
+```
+
+or call it using your webserver
+
+```
+http://yourwebserver/kajona/install.php
+```
+
+The script downloads the latest nightly build into the current directory (the file is downloaded only once).
+The zip-file is extracted to ```kajona```.
+
+After installation, you may log into the system using
+
+Username: admin
+Password: demo
+
--- install.php
+++ install.php
@@ -1,12 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sidler
- * Date: 03.11.14
- * Time: 16:54
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Stefan Idler, Idler IT-Services, http://www.mulchprod.de
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
  */
 
+
+// Config Params
 const DOWNLOAD_WEBSITE = "https://www.kajona.de/nightly-builds.html";
+const DOWNLOAD_FILE_REGEX = '#<a href="(([a-zA-Z0-9/_\-\:\.])*(\.zip))+">(kajona_v4_allinone_([0-9\-])*\.zip)</a>#i';
+// /Config Params
+
+
+
+echo "Kajona Nightly Build Installer\n";
+echo "(c) sidler@kajona.de";
+echo "\n\n";
 
 echo "Checking kajona.de for the latest nightly build\n";
 $strSite = file_get_contents(DOWNLOAD_WEBSITE);
@@ -16,7 +45,7 @@ if($strSite === false || $strSite == "") {
 }
 
 $arrLinks = array();
-if(preg_match('#<a href="(([a-zA-Z0-9/_\-\:\.])*(\.zip))+">(kajona_v4_allinone_([0-9\-])*\.zip)</a>#i', $strSite, $arrLinks) == 0) {
+if(preg_match(DOWNLOAD_FILE_REGEX, $strSite, $arrLinks) == 0) {
     echo "Error fetching the latest download-url\n";
     return;
 }
@@ -36,6 +65,15 @@ else {
 
     if(!is_file(__DIR__."/".$strZipname)) {
         echo "Download failed!";
+        return;
+    }
+}
+
+echo "Checking for present kajona-folder\n";
+if(is_dir(__DIR__."/kajona")) {
+    echo "Renaming existing kajona-folder\n";
+    if(!rename(__DIR__."/kajona", __DIR__."/kajona_".time())) {
+        echo "Failed to rename existing kajona folder, aborting.\n";
         return;
     }
 }
@@ -85,14 +123,16 @@ chdir(__DIR__."/kajona");
 echo "Including boostrap.php\n";
 require_once __DIR__.'/kajona/core/bootstrap.php';
 
+//fetch all output
 ob_start();
 require_once __DIR__.'/kajona/core/module_installer/installer.php';
-$data = ob_get_clean();
+$strData = ob_get_clean();
+//and remove it :)
 @ob_end_clean();
 
 echo "Setting up admin-account\n";
 class_carrier::getInstance()->getObjSession()->setSession("install_username", "admin");
-class_carrier::getInstance()->getObjSession()->setSession("install_password", "kajona");
+class_carrier::getInstance()->getObjSession()->setSession("install_password", "demo");
 class_carrier::getInstance()->getObjSession()->setSession("install_email", "demo@kajona.de");
 
 
@@ -106,4 +146,6 @@ echo "\n";
 echo "Finished!\n";
 echo "Open your browser and log in:\n\n";
 echo "Username: admin\n";
-echo "Password: kajona\n";
+echo "Password: demo\n";
+echo "\n";
+echo "Have fun using Kajona!\n";
